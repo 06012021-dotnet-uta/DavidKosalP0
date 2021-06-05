91d03ae02aec24dd73a0535e5f1730b1911ae5f9
DavidKosal <david.kosal@revature.net>
2021-06-05 14:05:34 -0500

    added comments

--- HackerRank/2D Array/Program.cs
+++ HackerRank/2D Array/Program.cs
@@ -34,24 +34,39 @@ class Result
         int j;
         int maxSum = 0;
         
+        //moves the hourglass down by 1 row
         for(row = 0; row < 4; row++){
             
+            //moves the hourglass right by 1 column
             for(column = 0; column < 4; column++){
+
+                //makes the shape of the hourglass at [0,0]
+                //adds the sum of the hourglass
                 sum = arr[x][y] + arr[x][y+1] + arr[x][y+2] +
                     arr[x+1][y+1] + arr[x+2][y] + arr[x+2][y+1] +
                     arr[x+2][y+2];
+
+                //stores the sum in an array    
                 hourglass[i] = sum;
                 i++;
+
+                //helps move the hourglass right by 1 column
+                //resets the sum for the next hourglass
                 y++;
                 sum = 0;
                 }
-                
+
+            //helps move the hourglass down by 1 row
+            //resets the column position to 0    
             x++;
             y=0;
         }
         
-        
+        //finds the highest sum of the hourglass
         for(j = 0; j < hourglass.Length; j++){
+            
+            //if the current hourglass is higher than maxSum,
+            //change the maxSum to current hourglass
             if(hourglass[j] > maxSum){
                 maxSum = hourglass[j];
                 
